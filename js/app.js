3f52a47526fe9600b2b5ccd01cdc77b428a80bf1
Jacob Moyle <jacobmoyle@gmail.com>
2015-06-20 17:53:22 -0700

    added more comments

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
+// Unsure about this
+// ***********************************
 // var ref = new Firebase("https://pricetracker2015.firebaseio.com/");
+
+// The following two lines are globally scoped. These are initialized as empty and populated once facebook OAuth succeeds.
+// ***********************************
 // var fbData;
 // var userData;
+
+// Unsure about this
+// ***********************************
 // var baseUrl = 'http://localhost:3000/'
 // var baseUrl = 'https://calm-island-3256.herokuapp.com/'
 
 $(document).ready(function(){
   console.log("hello")
+
+// Unsure about this
+// ***********************************
 //   $('#login').on('click', function(e){
 //     e.preventDefault();
 
+// Unsure about this
+// ***********************************
 //     fbAuth().then(function(authData){
 //       fbData = authData;
 //       var fbString = JSON.stringify(authData);
@@ -18,7 +31,10 @@ $(document).ready(function(){
 //     });
   });
 
-// //function definitions only ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Unsure about alllllll this
+// ***********************************
+
+//function definitions only ++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 // var fbAuth = function(){
 //   var promise = new Promise(function(resolve, reject){
