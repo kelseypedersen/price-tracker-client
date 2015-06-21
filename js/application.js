07d17ddd30c106153e79814ae21f34427e6ce86f
Mary Wallace <wallaceemee@gmail.com>
2015-06-21 14:27:17 -0700

    preventing default

--- js/application.js
+++ js/application.js
@@ -1,1 +1,24 @@
+$(document).ready(function(){
+  submitSearch();
+}); 
 
+var submitSearch = function(){
+  $("#product-search").on('submit', function(event){
+    event.preventDefault();
+    console.log("clicking the form");
+  });
+};
+
+
+
+
+
+
+
+
+  // <div class="search-product-form">
+  //   <form id="product-search" action="/products/results" method= "GET">
+  //     <input type="text" name="search"/>
+  //     <input type="submit" value="Search" />
+  //   </form>
+  // </div>
