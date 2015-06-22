0b9f282ba7f318c32c2249e183413444d263b415
Kelsey Pedersen <kelseypedersen@gmail.com>
2015-06-22 11:20:10 -0700

    Combine the Firebase and index page JS into app.js

--- js/app.js
+++ js/app.js
@@ -7,6 +7,9 @@ var userData;
 
 $(document).ready(function(){
   begin();
+  // add Mary's js code
+  hardLanding();
+  submitSearch();
 });
 
 // +++++++++++++++++++++++++ function definitions only +++++++++++++++++++++++++
@@ -55,3 +58,61 @@ var ajaxLogin = function(authData){
     alert("Login Failed");
   });
 };
+
+// Add Mary's JS //
+
+var hardLanding = function(){
+  $('a').on('click', function(event){
+    event.preventDefault();
+
+  $('a').remove();
+  $(".search-product-form").css("display", "block");
+
+  var request = $.ajax({
+    url:"http://localhost:3000/products/newest_products",
+    crossDomain: true,
+    type:"GET"
+   });
+
+    request.done(function(data){
+      var products = data["products"]
+
+      for(i = 0; i < products.length; i++){
+        $(".softLanding").append("<div class='product'><a href='" + products[i].clickUrl + "'>" + "<img src='" + products[i].image.sizes.IPhoneSmall.url + "' alt='product Image'>" + "</a></div>")
+      };
+    });
+  });
+};
+
+
+var submitSearch = function(){
+  $("#product-search").on('submit', function(event){
+    event.preventDefault();
+
+   var request = $.ajax({
+    url:"http://localhost:3000/products/results",
+    data: $(this).serialize(),
+    crossDomain: true,
+    type:"GET"
+   });
+
+   request.done(function(data){
+    debugger
+    console.log("successssssss");
+    $(".softLanding").remove();
+    var products = data["products"]
+
+
+    for(i = 0; i < products.length; i++){
+      $(".search-results").prepend("<div class='product'><a href='" + products[i].clickUrl + "'>" + "<img src='" + products[i].image.sizes.IPhoneSmall.url + "' alt='product Image'>" + "</a></div>")
+    };
+
+   });
+
+   request.fail(function(data){
+    console.log("fail");
+   });
+  });
+};
+
+
