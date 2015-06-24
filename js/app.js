b90793f6c4fee9e85ab6733d0430d2d83de0b03c
Danielle Rigsby <drigsby@hotmail.com>
2015-06-24 16:35:46 -0700

    jacob fucked up

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
   begin();
   submitSearch();
   formHandler();
-  showWishlist
+  // showWishlist
 });
 
 // +++++++++++++++++++++++++ function definitions only +++++++++++++++++++++++++
