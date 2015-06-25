c3f835bbb95ff6f5ceeb6fbd1d9b1b95b6f5cbcd
Jacob Moyle <jacobmoyle@gmail.com>
2015-06-25 11:37:31 -0700

    bug fixes.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var ref = new Firebase("https://pricetracker2015.firebaseio.com/");
-// var baseUrl = 'http://localhost:3000/'
-var baseUrl = 'http://young-ravine-5515.herokuapp.com/'
+var baseUrl = 'http://localhost:3000/'
+// var baseUrl = 'http://young-ravine-5515.herokuapp.com/'
 
 var fbData;
 var userData;
@@ -74,7 +74,7 @@ var populateWishList = function(){
 };
 
 var showWishlist = function(){
-  $(".nav-wishlist").on("click", function(event){
+  $("#nav-wishlist").on("click", function(event){
     event.preventDefault();
 
     populateWishList();
